83006a99c52e61b830177e230db36d75e6120365
Amanda Wang <Amanda@email.com>
2020-04-30 16:27:14 +0800

    commit buddyList

--- src/views/buddyList.vue
+++ src/views/buddyList.vue
@@ -1,19 +1,121 @@
 <template>
 	<div class="buddyList">
-		<van-button icon="star-o" type="primary" />
-		<van-button icon="star-o" type="primary">按钮</van-button>
-		<van-button icon="https://img.yzcdn.cn/vant/logo.png" type="info">按钮</van-button>
+		<!-- 联系人列表 -->
+		 <van-contact-list
+		    v-model="chosenContactId"
+		    :list="list"
+		    @add="onAdd"
+		    @edit="onEdit"
+		    @select="onSelect"
+		  />
+		<van-popup v-model="showEdit" position="bottom">
+		  <van-contact-edit
+		    :contact-info="editingContact"
+		    :is-edit="isEdit"
+		    @save="onSave"
+		    @delete="onDelete"
+		  />
+		</van-popup>
+		
 	</div>
 </template>
 
 <script>
-// import {Button} from 'vant';
+import { ContactList,Toast,ContactEdit,Popup} from 'vant'
+import axios from 'axios'
 export default {
 	name: 'buddyList',
 	components: {
-		// [Button.name]: Button
+		[ContactList.name]: ContactList,
+		[ContactEdit.name]: ContactEdit,
+		[Popup.name]: Popup
+	},
+	data(){
+		return {
+			list:[],
+			instance:null,  //axios实例
+			showEdit:false,  //编辑弹框显隐
+			editingContact:{}, //正在编辑的联系人数据
+			isEdit:false,       //新建false 编辑 true
+			chosenContactId: null,
+			showEdit: false
+		}
+	},
+	created() {
+		this.instance = axios.create({
+			baseURL:'http://localhost:9000/api',
+			timeout:1000
+		})
+		this.getList()
+	},
+	methods:{
+		getList(){
+			this.instance.get('/contactList').then(res=>{
+				this.list = res.data.data
+			}).catch(err=>{
+				Toast('请求失败，请稍后重试');
+				console.log(err)
+			})
+		},
+		onAdd(){
+			this.showEdit = true
+			this.isEdit = false
+		},
+		onEdit(info){
+			this.showEdit = true
+			this.isEdit = true
+			this.editingContact = info
+		},
+		onSave(info){
+			if(this.isEdit){
+				//编辑保存
+				this.instance.put('/contact/edit',info).then(res=>{
+					if(res.data.code === 200){
+						Toast('保存成功');
+						this.showEdit = false
+						this.getList()
+					}
+				}).catch(()=>{
+					Toast('请求失败，请稍后重试');
+				})
+			}else{
+				//新建保存
+				this.instance.post('/contact/new/json',info).then(res=>{
+					if(res.data.code === 200){
+						Toast('新建成功');
+						this.showEdit = false
+						this.getList()
+					}
+				}).catch(()=>{
+					Toast('请求失败，请稍后重试');
+				})
+			}
+		},
+		onDelete(info){
+			this.instance.delete('/contact',{
+				params:{
+					id:info.id
+				}
+			}).then(res=>{
+				if(res.data.code === 200){
+					Toast('删除成功');
+					this.showEdit = false
+					this.getList()
+				}
+			}).catch(()=>{
+				Toast('请求失败，请稍后重试');
+			})
+		},
+		// 选中联系人
+		onSelect() {
+			this.showList = false;
+		},
 	}
 };
 </script>
 
-<style></style>
+<style scoped>
+	.van-popup{
+		height: 100%;
+	}
+</style>
