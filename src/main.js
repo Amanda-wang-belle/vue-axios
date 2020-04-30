b8f8dc6b415bf688566f1c8b9d7f45f908fb040e
Amanda Wang <Amanda@email.com>
2020-04-30 17:51:56 +0800

    http-vue

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Http from './service/http'
 
 Vue.config.productionTip = false
 
+// 把Http挂在到vue实例上
+vue.prototype.$Http = Http
 new Vue({
   router,
   render: h => h(App)
